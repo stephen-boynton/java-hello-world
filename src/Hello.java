080ca25bb4a032d8a8e33250cb69e6f47a0e3c21
Stephen Boynton <boyntonsc@gmail.com>
2017-10-08 10:37:17 -0400

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by stephenboynton on 6/26/17.
+ */
+public class Hello {
+    public static void main (String[] args) {
+        System.out.println("Hello, Stephen!");
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+    }
+}
